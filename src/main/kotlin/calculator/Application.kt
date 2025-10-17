30efd072e558a4141c41110d47674397f1bfcc8f
110lily <anmy121212@gmail.com>
2025-10-18 01:38:09 +0900

    feat : 입력값 처리 함수 분리 및 기본 구분자(쉼표, 콜론) 분리 기능 추가
    입력 문자열을 읽어 빈 문자열 여부를 판단하고, 쉼표(,)와 콜론(:)을 기준으로 분리되도록 구현.

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -4,4 +4,10 @@ import camp.nextstep.edu.missionutils.Console
 
 fun main() {
     val input = Console.readLine()
+    val result = addNumbers(input)
+}
+
+fun addNumbers(input: String?) {
+    if (input.isNullOrBlank()) return
+    val numbers = input.split(",", ":")
 }
