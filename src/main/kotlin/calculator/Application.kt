373c54e2db66749173c1ecb94d3ecadbf27130d5
110lily <anmy121212@gmail.com>
2025-10-18 01:44:38 +0900

    feat : 분리된 문자열의 숫자 합산 기능 추가
    쉼표(,)와 콜론(:)으로 분리된 숫자들을 정수로 변환하여 모두 더한 합계를 반환하도록 기능을 추가함.

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -5,9 +5,17 @@ import camp.nextstep.edu.missionutils.Console
 fun main() {
     val input = Console.readLine()
     val result = addNumbers(input)
+    println("결과: $result")
 }
 
-fun addNumbers(input: String?) {
-    if (input.isNullOrBlank()) return
+fun addNumbers(input: String?): Int {
+    if (input.isNullOrBlank()) return 0
     val numbers = input.split(",", ":")
+    var sum = 0
+
+    for (number in numbers) {
+        sum += number.toInt()
+    }
+
+    return sum
 }
